cae51c1ec3262a79e205db9471a8b0db9252c2fd
tjobi <108347286+tjobi@users.noreply.github.com>
2022-09-02 12:49:16 +0200

    add test for program (#1)
    Co-authored-by: tbru <tbru@itu.dk>

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -3,8 +3,18 @@ namespace MyApp.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Main_when_no_args()
     {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
 
+        // Act
+        var program = Assembly.Load(nameof(MyApp));
+        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        //Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Hello, World!");
     }
 }
