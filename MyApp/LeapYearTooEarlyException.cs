0344427002cfefb131147623800c5cf4b1aef41e
tbru <tbru@itu.dk>
2022-09-02 16:24:38 +0200

    implement error handling for program

--- MyApp/LeapYearTooEarlyException.cs
+++ MyApp/LeapYearTooEarlyException.cs
@@ -2,9 +2,8 @@ namespace MyApp;
 
 public class LeapYearTooEarlyException : Exception
 {
-    private string message;
     public LeapYearTooEarlyException(string? message = null)
+    : base(message ?? "Invalid year - leap years only apply to years following 1582")
     {
-        this.message = message ?? "Invalid year - leap years only apply to years following 1582";
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,11 +3,23 @@
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-for (string s = Console.ReadLine(); s != null; s = Console.ReadLine())
+var calculator = new LeapCalculator();
+
+for (string? s = Console.ReadLine(); s != null; s = Console.ReadLine())
 {
-    var year = int.Parse(s);
-    var result = new LeapCalculator().IsLeapYear(year);
+    try
+    {
+        var year = int.Parse(s);
+        var result = calculator.IsLeapYear(year);
 
-
-    Console.WriteLine(result ? "yay":"nay");
+        Console.WriteLine(result ? "yay" : "nay");
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("Error: input a number.");
+    }
+    catch (LeapYearTooEarlyException e)
+    {
+        Console.WriteLine(e.Message);
+    }
 }
