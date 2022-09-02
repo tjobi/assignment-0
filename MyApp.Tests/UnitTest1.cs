12cb5693400641fc1a61129d86d16125863f1b9d
tjobi <108347286+tjobi@users.noreply.github.com>
2022-09-02 13:54:18 +0200

    Created user interface for IsLeapYear (#4)
    Co-authored-by: tbru <tbru@itu.dk>

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -3,18 +3,47 @@ namespace MyApp.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Main_when_no_args()
+    public void Main_when_yay()
     {
         // Arrange
+        /*var data = String.Join("\n", new[]
+        {
+            "1920", "2000", "1900", "2012"
+        });*/
+        const string YEAR = "1920";
+        using var reader = new StringReader(YEAR);
+        Console.SetIn(reader);
+
         using var writer = new StringWriter();
         Console.SetOut(writer);
 
         // Act
         var program = Assembly.Load(nameof(MyApp));
-        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+        program.EntryPoint?.Invoke(null,new[]{Array.Empty<string>()});
 
         //Assert
         var output = writer.GetStringBuilder().ToString().TrimEnd();
-        output.Should().Be("Hello, World!");
+        output.Should().Be("yay");
+    }
+
+
+    [Fact]
+    public void Main_when_nay()
+    {
+        // Arrange
+        const string YEAR = "1900";
+        using var reader = new StringReader(YEAR);
+        Console.SetIn(reader);
+
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.Load(nameof(MyApp));
+        program.EntryPoint?.Invoke(null,new[]{Array.Empty<string>()});
+
+        //Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("nay");
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyApp;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+for (string s = Console.ReadLine(); s != null; s = Console.ReadLine())
+{
+    var year = int.Parse(s);
+    var result = new LeapCalculator().IsLeapYear(year);
+
+
+    Console.WriteLine(result ? "yay":"nay");
+}
